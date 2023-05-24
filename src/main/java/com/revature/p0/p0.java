4d443c787cf764bbea74679c38c1ebbffacd05a9
Chichior <katiehakala@icloud.com>
2023-05-23 17:49:41 -0700

    renames project to p0

--- src/main/java/com/revature/p0/p0.java
+++ src/main/java/com/revature/p0/p0.java
@@ -1,6 +1,6 @@
-package com.revature.app;
+package com.revature.p0;
 
-public class App {
+public class p0 {
   public static void main(String args[]) {
     System.out.println("Welcome to eCommerce App!");
   }
