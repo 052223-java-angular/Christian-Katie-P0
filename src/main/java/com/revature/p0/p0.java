8dbc71a3a0a746b55b0bbe65aa967caa09c40e4a
Chichior <katiehakala@icloud.com>
2023-05-23 19:14:17 -0700

    made screens, services, and util, connected to db

--- src/main/java/com/revature/p0/p0.java
+++ src/main/java/com/revature/p0/p0.java
@@ -1,7 +1,19 @@
 package com.revature.p0;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import com.revature.p0.services.RouterService;
+import com.revature.p0.utils.ConnectionFactory;
+
 public class p0 {
-  public static void main(String args[]) {
-    System.out.println("Welcome to eCommerce App!");
+  public static void main(String args[]) throws ClassNotFoundException, SQLException, IOException {
+    System.out.println(ConnectionFactory.getInstance().getConnection());
+    Scanner scan = new Scanner(System.in);
+    RouterService router = new RouterService();
+    // router.navigate("/home", scan);
+
   }
+
 }
