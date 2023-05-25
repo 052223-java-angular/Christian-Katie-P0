9d76962669bbc860fbbe53192f0ecee956efc1e5
Christian Fernandez <chrisfernandez326@gmail.com>
2023-05-25 18:39:03 -0400

    Added files to models

---
+++ src/main/java/com/revature/p0/models/OrderItems.java
@@ -0,0 +1,18 @@
+package com.revature.p0.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+
+public class OrderItems {
+    private String id;
+    private String order_history_id;
+}
