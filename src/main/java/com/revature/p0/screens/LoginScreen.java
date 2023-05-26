7ed563879be806070f0418daa945d94035c5804d
Chichior <katiehakala@icloud.com>
2023-05-26 12:09:12 -0700

    Started LoginDAO. Not complete yet.

---
+++ src/main/java/com/revature/p0/screens/LoginScreen.java
@@ -0,0 +1,13 @@
+package com.revature.p0.screens;
+
+import java.util.Scanner;
+
+public class LoginScreen implements Screen {
+
+    @Override
+    public void start(Scanner scanner) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
+    }
+
+}
