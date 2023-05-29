481fee7401d7d13ae8cb5515927fc7e4a576a452
Christian Fernandez <chrisfernandez326@gmail.com>
2023-05-29 15:54:41 -0400

    Code added to ShoppingCartDAO

--- src/main/java/com/revature/p0/daos/ShoppingCartDAO.java
+++ src/main/java/com/revature/p0/daos/ShoppingCartDAO.java
@@ -10,21 +10,29 @@ import com.revature.p0.utils.*;
 
 public class ShoppingCartDAO {
 
-    // Create a shopping cart to link to a user that exist
-    public ShoppingCart addShopping_Cart(ShoppingCart shoppingCart) {
+     /*
+     * @param showShoppingCart(shopping cart) is connecting to the local database and shows all the items
+     *  that a user has in their shopping cart. If the cart is empty then it will say the shopping cart is
+     *  empty.
+     * 
+     * @return the related exception message should anything happen at runtime.
+     * If the method runs correctly, it will return all items in the user's cart.
+     * 
+     * 
+     * @author Christian Fernandez
+     */
+
+    public ShoppingCart showShoppingCart(ShoppingCart shoppingCart) {
 
         // connecting to data base
         try (Connection connection = ConnectionFactory.getInstance().getConnection()) {
-            String sql = "INSERT INTO shopping_cart (id, user_id) VALUES (?, ?)";
+            String sql = "SELECT product_id FROM cart_items WHERE shopping_cart_id = ?"; // getting products
 
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 try (ResultSet rs = ps.executeQuery()) {
-                    if (rs.next()) {
-                        ShoppingCart sc = new ShoppingCart();
-                        sc.setId(rs.getString("id"));
-                        sc.setUser_id(rs.getString("user_id"));
+                    while (rs.next()) {
+                        ShoppingCart sc = new ShoppingCart(rs.getString("id"), rs.getString("user_id"));
                         return sc; // Might need to return Optional.of(sc)
-                        // Comment
                     }
                 }
             }
